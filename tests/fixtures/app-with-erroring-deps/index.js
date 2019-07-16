136f22a6cff33aef52ab1fd12196c8abd92cd031
Joe Bateson <joe@joebateson.com>
2019-07-16 10:58:06 -0700

    Warn if scripts are missing (#7)

---
+++ tests/fixtures/app-with-erroring-deps/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved */
+const someLibraryWithErroringScript = require("some-library-with-erroring-script");
+
+module.exports = { someLibraryWithErroringScript };
